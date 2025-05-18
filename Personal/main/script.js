551bbd1229df1dd31cab68a2d49df13ef6a0d657
YBJIN0902 <0974227977jin@gmail.com>
2025-05-18 21:22:31 +0800

    Edit: message bot function complite Add: photo

--- Personal/main/script.js
+++ Personal/main/script.js
@@ -11,6 +11,20 @@ function scrollToTop() {
   }
 }
 
+function openLightbox(src) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+
+  lightboxImg.src = src;
+  lightbox.classList.add("show");
+}
+
+function closeLightbox() {
+  const lightbox = document.getElementById("lightbox");
+  lightbox.classList.remove("show");
+}
+
+
 function toggleFloatingMenu() {
   document.getElementById('fabButtons').classList.toggle('show');
 }
--- Personal/server.js
+++ Personal/server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const fetch = require('node-fetch'); // 或用 axios
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const PORT = 907;
 
-const WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1373326123240525925/Xyn0pOCuOVht13SNmCCokvwqKl8LKl5LY__5Yu4sjdz2aah42DB-EDZYolr5LEZiZWW7'
+const WEBHOOK_URL = 'https://discord.com/api/webhooks/1373326123240525925/Xyn0pOCuOVht13SNmCCokvwqKl8LKl5LY__5Yu4sjdz2aah42DB-EDZYolr5LEZiZWW7';
 
 app.use(bodyParser.json());
-
-// 靜態檔案位置（讓前端檔案可存取）
 app.use(express.static(path.join(__dirname, 'main')));
 
-// 根目錄導向 index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'main', 'index.html'));
 });
 
 app.post('/send-discord', async (req, res) => {
   const message = req.body.message;
-
   try {
     const response = await fetch(WEBHOOK_URL, {
       method: 'POST',
@@ -40,8 +42,6 @@ app.post('/send-discord', async (req, res) => {
     res.status(500).send('error');
   }
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`伺服器啟動：http://localhost:${PORT}`);
